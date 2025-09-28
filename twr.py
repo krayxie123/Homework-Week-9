74ba3de3006c06cc3fcca00f5df8bc7300bfb001
Kai Xie <kxie0010@student.monash.edu>
2025-09-29 04:08:02 +1000

    final edits

--- twr.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-school_locations = pd.read_csv('data/dv402-SchoolLocations2025.csv')
-
-#print(school_locations.head(10))
-
-vce_results = pd.read_csv('data/VCE 2024 Results.csv')
-print(f"VCE Results: {len(vce_results)} schools")
-print(vce_results['Median VCE study score'].value_counts().head(10))
